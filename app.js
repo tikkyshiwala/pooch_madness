d525eb6f889b0339265f553370237c90f498766c
John Brosnan <jbrosnan@constantcontact.com>
2014-02-15 10:28:05 -0500

    poochiness

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var path = require('path');
 var app = express();
 
 // all environments
+app.set('appName', 'Pooch Madness');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
