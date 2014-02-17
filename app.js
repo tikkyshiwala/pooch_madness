158f4e5e44cf6d99d0c7b69bacc883ccd298eb9b
John Brosnan <jbrosnan@constantcontact.com>
2014-02-17 08:34:24 -0500

    wip

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.session());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // development only
 if ('development' == app.get('env')) {
