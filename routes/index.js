48e468e3b16758f4aead069362eca4249aa48e98
Alex Berry <highwaybobbery@gmail.com>
2014-03-06 00:24:35 -0500

    added signin/signout

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@
 
 exports.index = function(req, res){
   var Bracket = require('../models/bracket');
+  name = req.cookies.name
   res.render('index', {
     title: res.app.get('appName'),
     bracket: Bracket.get()
+    name: name
   });
 };
